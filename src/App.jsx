ab95a13ddf06299fe209921c3c226f5c4e08858c
r-antoni <reindraantoni@gmail.com>
2023-06-17 06:30:52 +0700

    Bg update and destructuring props

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,47 @@
 import { useState } from 'react'
-import Task from './Task';
+import Task from './Task'
 
 function App() {
-  const [toDo, setToDo] = useState([]);
-  const [newTask, setNewTask] = useState("")
+  const [taskList, setTasklist]= useState([])
+  const [newTask, setNewTask]= useState("")
 
-  const handleChange  = (event) => {
+  const handleChange =(event)=>{
     setNewTask(event.target.value)
   }
-  
-  const addTask = () => {
-    const task = {
-      id: toDo.length === 0 ? 1 : toDo[toDo.length - 1].id + 1,
+  const addTask =()=>{
+    const taskData = {
+      id: taskList.length === 0 ? 1 : taskList[taskList.length -1].id +1,
       taskName: newTask,
-      completed: false,
+      isComplete: false,
     }
-    setToDo([...toDo, task])
+    setTasklist([...taskList, taskData])
   }
-  
-  const completeTask = (id) => {
-    setToDo(
-      toDo.map((task) =>{
-        if(task.id === id){
-          return {...task, completed: true}
-        }else{
-          return task
-        }
-      })
-    )
+  const deleteTask=(id)=>{
+    setTasklist(taskList.filter((task)=> task.id !== id))
   }
-
-  const deleteTask = (id) => {
-    setToDo(toDo.filter((task) =>{
-      return task.id !== id
+  const completeTask=(id)=>{
+    setTasklist(taskList.map((task)=>{
+      if(task.id === id){
+        return {...task, isComplete: true}
+      } else{
+        return task
+      }
     }))
   }
-
   return (
-    <div className="App flex flex-col w w-full items-center">
-
-      <div className="addTask flex justify-center items-center w-[100%] px-10 py-10 gap-5 bg-zinc-500">
-        <input className="border-2 border-slate-300 rounded-lg px-4 py-2 w-1/2" onChange={handleChange}/>
-        <button className="font-medium bg-slate-300 px-3 py-2 rounded-xl" onClick = {addTask}> Add Task </button>
+    <div className="flex flex-col gap-5 min-w-screen min-h-screen items-center bg-[url('./assets/bg.jpg')]">
+      <div className="flex justify-center items-center gap-5 px-10 py-10 w-full">
+        <input className="border-2 border-slate-900 rounded-lg px-4 py-2 w-1/4 font-medium" type="text" onChange={handleChange}/>
+        <button className="bg-slate-200 border border-slate-400 rounded-lg px-4 py-2 font-medium" onClick={addTask} >Add Task</button>
       </div>
-      
-      <div className="list flex flex-col items-center gap-10 py-6 h-auto w-[100%] ">
-        {toDo.map((task) =>{
+      <div className="flex flex-col justify-center items-center gap-5 w-full">
+        {taskList.map((task)=>{
           return (
-          <Task taskName={task.taskName} id={task.id} completed={task.completed} deleteTask={deleteTask} completeTask={completeTask}/>
-          )
-        })}
+          <Task taskName={task.taskName} id={task.id} isComplete={task.isComplete} deleteTask={deleteTask} completeTask={completeTask}/>
+          )})}
       </div>
     </div>
-  );
+  )
 }
-
 
 export default App
--- src/Task.jsx
+++ src/Task.jsx
@@ -1,12 +1,10 @@
 const Task = (props) => {
-    return ( 
-        <div className="flex items-center gap-8 font-semibold text-xl">
-            <h1 className={props.completed ? 'bg-green-200 px-5 py-2 rounded-xl': 'bg-red-200 px-5 py-2 rounded-xl'}>{props.taskName}</h1>
-            <div className="flex items-center gap-2">
-                <button className="font-medium text-sm bg-slate-300 px-3 py-2 rounded-xl" onClick={ ()=> props.completeTask(props.id)} >Complete</button>
-                <button className="font-medium text-sm bg-slate-300 px-3 py-2 rounded-xl" onClick={ ()=> props.deleteTask(props.id) }>Delete</button>
-            </div>
-            
+    const{taskName, id, isComplete, deleteTask, completeTask} = props
+    return (
+        <div className="flex justify-center items-center gap-3 px-5 py-3 w-min-screen">
+            <h1 className={isComplete ? 'bg-green-200 px-6 py-3 rounded-lg line-through font-medium': 'bg-red-200 px-6 py-3 rounded-lg font-medium'}>{taskName}</h1>
+            <button className="bg-slate-100 border border-slate-400 rounded-lg px-4 py-2 font-medium text-sm ml-5" onClick={()=> completeTask(id)}>Complete</button>
+            <button className="bg-slate-100 border border-slate-400 rounded-lg px-6 py-2 font-medium text-sm" onClick={()=> deleteTask(id)}>Delete</button>
         </div>
     );
 }
